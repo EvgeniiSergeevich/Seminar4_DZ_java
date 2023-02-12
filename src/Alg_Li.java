ee63d5d9448ba4c73d17bb23d192412758738f98
Evgeniy <zheinirk38@gmail.com>
2023-02-12 16:57:16 +0800

    Сделал один метод для заполнения поля
    Удалил лишние методы

--- src/Alg_Li.java
+++ src/Alg_Li.java
@@ -18,10 +18,11 @@ public class Alg_Li {
         ArrayDeque<String> states = new ArrayDeque<String>();
         Scanner key = new Scanner(System.in);
         int[][] boardLi = board(key);                               // Игровое поле
-        int[] start = enterPoint(key);                              // Координаты точки старта
+        int[] start = markPoints(key, "старта");                              // Координаты точки старта
         int[] finishPoint = new int[2];                             // Координаты точки финиша
+        int[] walls = new int[2];                                   // Координаты стены
         boardLi[start[1]][start[0]] = 1;                            // Помечаю ячейку старта "1"
-        System.out.println("Введите количество точек выхода(финиша)");
+        System.out.print("Введите количество точек выхода(финиша): ");
         int finPointsAmount = 0;                                    // Количество точек финиша
 
         if(key.hasNextInt())
@@ -35,22 +36,43 @@ public class Alg_Li {
 
         while (finPointsAmount != 0)                                // Отмечаю точки финиша "-2"
         {
-            finishPoint = finishPoint(key);
+            finishPoint = markPoints(key, "финиша");
             boardLi[finishPoint[1]][finishPoint[0]] = -2;
             finPointsAmount--;
         }
+
+
+        int wallAmount = 0;                                         // Создание стен
+        System.out.print("Ввдите количество стен: ");
+        if(key.hasNextInt())
+        {
+            wallAmount = key.nextInt();
+        }
+        else
+        {
+            System.out.println("Вы ввели не число!");
+        }
+        System.out.println(wallAmount + " WALL");
+
+        while (wallAmount != 0)                                // Отмечаю точки финиша "-1"
+        {
+            walls = markPoints(key, "стен");
+            boardLi[walls[1]][walls[0]] = -1;
+            wallAmount--;
+        }
+
         for (int i = 0; i < boardLi.length; i++)                    // Вывожу на экран Игровое поле
         {
             for (int j = 0; j < boardLi[i].length; j++)
             {
-                System.out.print(boardLi[i][j] + "  ");
+                System.out.print(boardLi[i][j] + "   ");
             }
             System.out.println();
         }
     }
 
 
-    public static int[][] board(Scanner key)                    // Создаю квадратного поля, размером size
+    public static int[][] board(Scanner key)                                // Создаю квадратного поля, размером size
     {
         System.out.print("Укажите размер поля: ");
         int size = consoleIn(key);
@@ -58,21 +80,11 @@ public class Alg_Li {
     }
 
 
-    public static int[] enterPoint(Scanner key)                 // Создаю точку входа
+    public static int[] markPoints(Scanner key, String str)                // Отмечаю точки на поле
     {
-        System.out.print("Укажите координату X точки старта (считая первую клетку, как 1,1): ");
+        System.out.printf("Укажите координату X точки %s (считая первую клетку, как 1,1): ", str);
         int x = consoleIn(key) - 1;
-        System.out.print("Укажите координату Y точки старта (считая первую клетку, как 1,1): ");
-        int y = consoleIn(key) - 1;
-        return new int[]{x, y};
-    }
-
-
-    public static int[] finishPoint(Scanner key)                // Создаю точку выхода
-    {
-        System.out.print("Укажите координату X точки финиша (считая первую клетку, как 1,1): ");
-        int x = consoleIn(key) - 1;
-        System.out.print("Укажите координату Y точки финиша (считая первую клетку, как 1,1): ");
+        System.out.printf("Укажите координату Y точки %s (считая первую клетку, как 1,1): ", str);
         int y = consoleIn(key) - 1;
         System.out.println();
         return new int[]{x, y};
